bfa0d20d369c5525a9039fe850814ec45d6faef0
MajdAssad22 <83403902+MajdAssad22@users.noreply.github.com>
2022-05-25 18:16:35 +0300

    Added basic components and routing and homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewOrderComponent } from './components/new-order/new-order.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: "Home", component: HomeComponent},
+  { path: "Login", component: LoginComponent},
+  { path: "NewOrder",  component: NewOrderComponent},
+  { path: "Orders",  component: OrdersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NewOrderComponent } from './components/new-order/new-order.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    NewOrderComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
